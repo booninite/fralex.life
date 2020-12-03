12c9cf8fc3b44913f19238f9b709111c65b78caf
alex weidner <shimmerjs@dpu.sh>
2020-12-02 20:39:04 -0500

    WIP implementing pix fetching from google photos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,35 @@
 import '../styles/globals.css'
+import axios from 'axios';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+export async function getStaticProps() {
+  const regex = /\["(https:\/\/lh3\.googleusercontent\.com\/[a-zA-Z0-9\-_]*)"/g
+  const albumId = "gm34wGiEgG9Q48yD6"
+
+  function extractPhotos(content) {
+   const links = new Set()
+    let match
+    while (match = regex.exec(content)) {
+      links.add(match[1])
+    } 
+    return Array.from(links)
+  }
+  
+  async function getAlbum(id) {
+    const response = await axios.get(`https://photos.app.goo.gl/${id}`)
+    return extractPhotos(response.data)
+  }
+  
+  const pix = await getAlbum(albumId)
+  return {
+    props: {
+      pix,
+    }
+  }
 }
 
 export default MyApp
--- pages/api/pix.ts
+++ pages/api/pix.ts
@@ -1,6 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios';
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+const albumId = "gm34wGiEgG9Q48yD6"
+
+export default async (req, res) => {
+  try {
+    const results = await getAlbum(albumId)
+    res.statusCode = 200
+    res.json(results)
+  } catch (e) {
+    res.status(500)
+  }
+}
+
+const regex = /\["(https:\/\/lh3\.googleusercontent\.com\/[a-zA-Z0-9\-_]*)"/g
+
+function extractPhotos(content) {
+ const links = new Set()
+  let match
+  while (match = regex.exec(content)) {
+    links.add(match[1])
+  } 
+  return Array.from(links)
+}
+
+async function getAlbum(id) {
+  const response = await axios.get(`https://photos.app.goo.gl/${id}`)
+  return extractPhotos(response.data)
 }
